b4e02022085827e57fb8cf8a36867da0eacc7577
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 17:18:37 +0200

    fix array numbe to print

--- coda-array/js/app.js
+++ coda-array/js/app.js
@@ -3,6 +3,15 @@ const list = ['acqua', 'lattte', 'yogurt', 'coca-cola', 'pasta', 'farina', 'caff
 console.log(list);
 
 // stampa gli ultimi 5 elementi dell'array
-for (let i = (list.length) - 1 ; i >= 3; i-- ) {
+for (let i = list.length - 1; i >= list.length - 5; i-- ) {
     console.log(i, list[i]);
+}
+
+// creare una lista di elementi
+const list2 = ['acqua', 'lattte', 'yogurt', 'coca-cola'];
+console.log(list2);
+
+// stampa gli ultimi 5 elementi dell'array
+for (let i = list2.length - 1; i >= list2.length - 5; i-- ) {
+    console.log(i, list2[i]);
 }
