0f6d4f6aa6cedebb6e5bb1946bdcf4e127a55024
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 17:37:48 +0200

    print element in page

--- coda-array/js/app.js
+++ coda-array/js/app.js
@@ -1,17 +1,15 @@
+const ulElement = document.querySelector('p');
+let liContent = '';
+
 // creare una lista di elementi
 const list = ['acqua', 'lattte', 'yogurt', 'coca-cola', 'pasta', 'farina', 'caffè', 'biscotti'];
 console.log(list);
 
 // stampa gli ultimi 5 elementi dell'array
 for (let i = list.length - 1; i >= list.length - 5; i-- ) {
-    console.log(i, list[i]);
+    const currentElement = list[i];
+    liContent += currentElement + ' ';
+    console.log(currentElement);
 }
 
-// creare una lista di elementi
-const list2 = ['acqua', 'lattte', 'yogurt', 'coca-cola'];
-console.log(list2);
-
-// stampa gli ultimi 5 elementi dell'array
-for (let i = list2.length - 1; i >= list2.length - 5; i-- ) {
-    console.log(i, list2[i]);
-}
+ulElement.append(liContent);
