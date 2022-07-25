31d57a836d8cbfea7eeaa230182561ca65ea37bb
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 15:25:12 +0200

    print message in page

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,7 +1,9 @@
 const mailList = ['luca@gmail.com', 'marco@gmail.com', 'paolo@gmail.com'];
 // chiedere all'utente la sua mail
 const userMail = prompt('Inserisci la tua e-mail.');
+const messageElement = document.querySelector('p');
 let isInList;
+let messageContent;
 
 // controllare se la mail è nella lista
 for (let i = 0; i < mailList.length; i++) {
@@ -15,9 +17,11 @@ for (let i = 0; i < mailList.length; i++) {
 // SE la mail è nella lista
 if (isInList) {
     // stampare il messaggio Accesso Consentito
-    console.log('Accesso Consentito');
+    messageContent = 'Benvenuto nel tuo Account Google';
 } else {
 // ALTRIMENTI
     // stampare il messaggio Accesso Negato
-    console.log('Accesso Negato');
+    messageContent = 'Siamo spiacenti, la tua email non è valia.';
 }
+
+messageElement.append(messageContent);
