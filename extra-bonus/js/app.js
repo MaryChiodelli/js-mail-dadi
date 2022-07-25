4303fa417ea8dfcc52bc2fe1cb11daa8dad9d64e
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 18:15:30 +0200

    add base functionality

--- extra-bonus/js/app.js
+++ extra-bonus/js/app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+// chiedere all'utente quanti elementi dovrà contenere l'array
+let userCounter = parseInt(prompt('Di qunati numeri hai bisogno?'));
+userCounter = Math.abs(userCounter);
+
+// creare array di numeri casuali in base al numero ricevuto
+let randomNumberList = [];
+
+if (isNaN(userCounter)) {
+    alert('I dati inseriti non sono corretti.')
+} else {
+    for (let i = 0; i < userCounter; i++) {
+        const randomNumber = Math.floor( Math.random() * 100 ) + 1;
+        randomNumberList.push(randomNumber);
+    }
+    
+    console.log(userCounter, randomNumberList)
+}
