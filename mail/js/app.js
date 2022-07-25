daa4afd85823afe260ca2ceab945b51ea6d7e8eb
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 15:16:45 +0200

    add mail check

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const mailList = ['luca@gmail.com', 'marco@gmail.com', 'paolo@gmail.com'];
+// chiedere all'utente la sua mail
+const userMail = prompt('Inserisci la tua e-mail.');
+let isInList;
+
+// controllare se la mail è nella lista
+for (let i = 0; i < mailList.length; i++) {
+    const currentMail = mailList[i];
+
+    if (currentMail === userMail) {
+        isInList = true;
+    }
+}
+
+// SE la mail è nella lista
+if (isInList) {
+    // stampare il messaggio Accesso Consentito
+    console.log('Accesso Consentito');
+} else {
+// ALTRIMENTI
+    // stampare il messaggio Accesso Negato
+    console.log('Accesso Negato');
+}
