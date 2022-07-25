96583050e7d1cd7c9e89aae9e6def5a331434b45
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 16:14:11 +0200

    print score in page

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -1,20 +1,29 @@
+const buttonElement = document.querySelector('button');
+const messageElement = document.querySelector('div');
+let message;
+
 // genera un numero random da 1 a 6 per il giocatore
 const playerRandomNumber = Math.floor( Math.random() * 5 ) + 1;
 // genera un numero random da 1 a 6 per il computer
 const computerRandomNumber = Math.floor( Math.random() * 5 ) + 1;
 
-console.log(playerRandomNumber, computerRandomNumber);
+buttonElement.addEventListener('click', function () {
+    // SE numero del giocatore > numero del computer
+    if (playerRandomNumber > computerRandomNumber) {
+        // stampare in console Hai vinto
+        message = 'Hai vinto';
+    } else if (playerRandomNumber < computerRandomNumber) {
+    // ALTRIMENTI SE numero del giocatore < numero del computer
+        // stampare in console Hai perso
+        message = 'Hai perso';
+    } else {
+    // ALTRIMENTI
+        // stampare in console Pareggio
+        message = 'Pareggio';
+    }
 
-// SE numero del giocatore > numero del computer
-if (playerRandomNumber > computerRandomNumber) {
-    // stampare in console Hai vinto
-    console.log('Hai vinto');
-} else if (playerRandomNumber < computerRandomNumber) {
-// ALTRIMENTI SE numero del giocatore < numero del computer
-    // stampare in console Hai perso
-    console.log('Hai perso');
-} else {
-// ALTRIMENTI
-    // stampare in console Pareggio
-    console.log('Pareggio');
-}
+    messageElement.innerHTML = `
+    <h1>${message}</h1>
+    <p>In tuo numero è: ${playerRandomNumber}</p>
+    <p>Il numero del computer è: ${computerRandomNumber}</p>`;
+})
