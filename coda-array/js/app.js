8995e005f1a310dc154671eab658b8cdb11d43e1
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-25 17:44:00 +0200

    fix min lengh array

--- coda-array/js/app.js
+++ coda-array/js/app.js
@@ -3,13 +3,15 @@ let liContent = '';
 
 // creare una lista di elementi
 const list = ['acqua', 'lattte', 'yogurt', 'coca-cola', 'pasta', 'farina', 'caffè', 'biscotti'];
-console.log(list);
 
 // stampa gli ultimi 5 elementi dell'array
 for (let i = list.length - 1; i >= list.length - 5; i-- ) {
     const currentElement = list[i];
-    liContent += currentElement + ' ';
-    console.log(currentElement);
+
+    if (currentElement !== undefined) {
+        liContent += currentElement + ' ';
+        console.log(i, currentElement);
+    }
 }
 
 ulElement.append(liContent);
